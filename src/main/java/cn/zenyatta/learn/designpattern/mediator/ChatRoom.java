7b41580f97b46ca0a08d08216a25146a05042095
mingming.song <dustinsong@outlook.com>
2019-07-05 18:10:16 +0800

    中介者模式

--- src/main/java/cn/zenyatta/learn/designpattern/mediator/ChatRoom.java
+++ src/main/java/cn/zenyatta/learn/designpattern/mediator/ChatRoom.java
@@ -7,6 +7,6 @@ import java.util.Date;
  */
 class ChatRoom {
     public static void showMessage(User user, String message){
-        System.out.println(new Date().toString()+" ["+user.getName()+" ] :"+message);
+        System.out.println(new Date().toString()+" ["+user.getName()+"] :"+message);
     }
 }
